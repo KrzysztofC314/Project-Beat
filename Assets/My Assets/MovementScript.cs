4e469496b4b23c169204825ce30b7b6209db065b
KrzysztofC314 <115410290+KrzysztofC314@users.noreply.github.com>
2022-10-18 14:00:07 +0200

    movement
    Made a player movement script, began work on a camera follow script

---
+++ Assets/My Assets/MovementScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    int direction = 1;
+    int lane = -1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right*Time.deltaTime*speed*direction);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            lane = lane * -1;
+            transform.position = new Vector2(transform.position.x, transform.position.y + lane*distance);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = 1;
+        }
+    }
+}
