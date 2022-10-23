63a554c713f35630f572d054f8a293fdd3ef7ac3
KrzysztofC314 <115410290+KrzysztofC314@users.noreply.github.com>
2022-10-23 22:49:20 +0200

    Zmieniono metode ruchu
    zmieniono metodę ruchu na bardziej beat em upową

--- Assets/My Assets/MovementScript.cs
+++ Assets/My Assets/MovementScript.cs
@@ -4,34 +4,24 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public float speed;
-    public float distance;
-    [HideInInspector]
-    public int direction = 1;
-    int lane = -1;
-    
-    // Start is called before the first frame update
+    public float verticalSpeed = 2.0f;
+    public float horizontalSpeed = 5.0f;
+    private Rigidbody2D rb;
+    Vector2 movement;
+
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right*Time.deltaTime*speed*direction);
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            lane = lane * -1;
-            transform.position = new Vector2(transform.position.x, transform.position.y + lane*distance);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            direction = -1;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            direction = 1;
-        }
+        movement.x = Input.GetAxis("Horizontal")*horizontalSpeed;
+        movement.y = Input.GetAxis("Vertical")*verticalSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
     }
 }
