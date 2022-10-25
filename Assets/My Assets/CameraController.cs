9d420f4525f3d2320d9b2a16e7c511986deae841
KrzysztofC314 <115410290+KrzysztofC314@users.noreply.github.com>
2022-10-25 15:37:02 +0200

    Edge Collider
    Dodalem edge collider

--- Assets/My Assets/CameraController.cs
+++ Assets/My Assets/CameraController.cs
@@ -16,7 +16,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position =new Vector3(player.transform.position.x + offset, transform.position.y, transform.position.z);
     }
