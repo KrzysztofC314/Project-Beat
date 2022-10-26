d10ccaaba92172301a52d1ae20a2b40940b27e3f
KrzysztofC314 <krzycie10@gmail.com>
2022-10-27 00:31:00 +0200

    Animacje
    zacząłem implementować animacje

--- Assets/My Assets/MovementScript.cs
+++ Assets/My Assets/MovementScript.cs
@@ -7,17 +7,21 @@ public class MovementScript : MonoBehaviour
     public float verticalSpeed = 2.0f;
     public float horizontalSpeed = 5.0f;
     private Rigidbody2D rb;
+    private Animator anim;
     Vector2 movement;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxis("Horizontal")*horizontalSpeed;
         movement.y = Input.GetAxis("Vertical")*verticalSpeed;
+        anim.SetFloat("horizontal", movement.x);
+        anim.SetFloat("speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
